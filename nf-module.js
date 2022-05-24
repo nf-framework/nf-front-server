ff0a9ac4dff6626dca1f1f28705a6b055a7f5cae
Azat Yamalov <yamalov.azat@live.com>
2022-05-24 10:01:07 +0300

    Registered route for static folder in modules

--- nf-module.js
+++ nf-module.js
@@ -1,21 +1,40 @@
 import { web } from "@nfjs/back";
-import { getCacheKey, prepareResponse, registerLibDir } from "./lib/server.js";
+import { getCacheKey, prepareResponse } from "./lib/server.js";
 import fs from "fs";
 
 import { extension } from "@nfjs/core";
-import path from 'path';
-const __dirname = path.join(path.dirname(decodeURI(new URL(import.meta.url).pathname))).replace(/^\\([A-Z]:\\)/, "$1");
+import mime from "mime";
+
+let staticPathsMap = new Map();
+
 
 async function init() {
-    registerLibDir('front', __dirname + '/static');
 
     web.on('GET', '/', {}, async context => {
-        //context.type('text/html');
         const requestPath = await extension.getFiles('static/_index.html');
         const customOptions = context.customOptions;
         const cacheKey = getCacheKey(requestPath, customOptions);
         const response = await prepareResponse(cacheKey,
             { customOptions, contentType: 'html', mimeType: 'text/html' },
+            () => {
+                return fs.createReadStream(requestPath);
+            });
+        context.headers(response.headers);
+        context.send(response.stream);
+    });
+
+    web.on('GET', '/static/*', { }, async context => {
+        let requestPath = null;
+        if(staticPathsMap.get(context.params['*'])) {
+            requestPath = staticPathsMap.get(context.params['*']);
+        } else {
+            requestPath = await extension.getFiles('static/' + context.params['*']);
+            staticPathsMap.set(context.params['*'], requestPath);
+        }
+        const customOptions = context.customOptions;
+        const cacheKey = getCacheKey(requestPath, customOptions);
+        const response = await prepareResponse(cacheKey,
+            { customOptions, mimeType: mime.getType(requestPath) },
             () => {
                 return fs.createReadStream(requestPath);
             });
