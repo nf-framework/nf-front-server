bf9bb384c23c6730bc821aa0ba71a3498eb41ddd
Dmitry Boikov <boikovdv@gmail.com>
2025-04-10 15:03:40 +0300

    code style fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const frontHooks = {
     modulePreProcessors: [],
     htmlPreProcessors: []
-}
+};
 
 export { frontHooks };
-export { prepareResponse, getCacheKey, registerLibDir, registerCustomElementsDir, customElements } from "./lib/server.js";
-export { MultiStream } from "./lib/multistream.js";
+export { prepareResponse, getCacheKey, registerLibDir, registerCustomElementsDir, customElements } from './lib/server.js';
+export { MultiStream } from './lib/multistream.js';
--- nf-module.js
+++ nf-module.js
@@ -1,17 +1,16 @@
-import { web } from "@nfjs/back";
-import { getCacheKey, prepareResponse } from "./lib/server.js";
-import fs from "fs";
-import { PassThrough } from "stream";
-import { MultiStream } from "./lib/multistream.js";
+import { web } from '@nfjs/back';
+import { getCacheKey, prepareResponse } from './lib/server.js';
+import fs from 'fs';
+import { PassThrough } from 'stream';
+import { MultiStream } from './lib/multistream.js';
 
-import { extension, config } from "@nfjs/core";
-import mime from "mime";
+import { extension, config } from '@nfjs/core';
+import mime from 'mime';
 
-let staticPathsMap = new Map();
-
+const staticPathsMap = new Map();
 
 async function init() {
-    web.on('GET', '/', {}, async context => {
+    web.on('GET', '/', {}, async (context) => {
         const requestPath = await extension.getFiles('static/_index.html');
         const customOptions = context.customOptions;
         const cacheKey = getCacheKey(requestPath, customOptions);
@@ -24,9 +23,9 @@ async function init() {
         context.send(response.stream);
     });
 
-    web.on('GET', '/static/*', { }, async context => {
+    web.on('GET', '/static/*', { }, async (context) => {
         let requestPath = null;
-        if(staticPathsMap.get(context.params['*'])) {
+        if (staticPathsMap.get(context.params['*'])) {
             requestPath = staticPathsMap.get(context.params['*']);
         } else {
             requestPath = await extension.getFiles('static/' + context.params['*']);
@@ -43,25 +42,25 @@ async function init() {
         context.send(response.stream);
     });
 
-    web.on('GET', '/nfjs-theme/', async context => {
+    web.on('GET', '/nfjs-theme/', async (context) => {
         const theme = (context.params.theme || config.theme || 'default').split(',');
-        let customOptions = context.customOptions;
-        let cacheKey = getCacheKey('nfjs-theme/' + theme, customOptions);
+        const customOptions = context.customOptions;
+        const cacheKey = getCacheKey('nfjs-theme/' + theme, customOptions);
         const response = await prepareResponse(cacheKey,
             { customOptions, minify: 'deny', contentType: 'css', mimeType: 'text/css' }, async () => {
-                let streams = [];
+                const streams = [];
                 for (let i = 0, c = theme.length; i < c; i++) {
-                    let styleFile = await extension.getFiles(`themes/${theme[i]}/style.css`, { invert: true });
+                    const styleFile = await extension.getFiles(`themes/${theme[i]}/style.css`, { invert: true });
                     if (styleFile) {
-                        let st = new PassThrough();
+                        const st = new PassThrough();
                         st.write(`\n/*nfjs-theme-${theme[i]}*/\n`);
                         st.end();
                         streams.push(st);
                         streams.push(fs.createReadStream(styleFile));
                     }
                 }
-                let stream = new PassThrough();
-                let mstream = new MultiStream(streams);
+                const stream = new PassThrough();
+                const mstream = new MultiStream(streams);
                 mstream.pipe(stream);
                 return stream;
             });
